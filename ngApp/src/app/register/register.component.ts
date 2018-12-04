29a1dc3f5126eb3220fceb0b83e62074062b1b95
Ramin <raminghaderi77@gmail.com>
2018-12-04 13:02:01 +0100

    Registration Service

--- ngApp/src/app/register/register.component.ts
+++ ngApp/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -8,13 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
   registerUserData = {};
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
   }
 
   registerUser() {
-    console.log(this.registerUserData);
+    this.auth.registerUser(this.registerUserData)
+      .subscribe(
+        res => console.log(res),
+        err => console.error(err)
+      );
   }
 
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const PORT = 3000;
 
@@ -7,7 +8,9 @@ const api = require('./routes/api');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
+
 
 app.use('/api', api);
 
